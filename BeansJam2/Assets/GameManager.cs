fdb86664e965b4c55e0b146269bed85302ed9643
Vestalia <Vestalia95>
2018-10-13 22:31:32 +0200

    balancebar ready

---
+++ BeansJam2/Assets/GameManager.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    void EndGame () 
+        {
+        Debug.Log("GAME OVER!");
+}
+
+}
---
+++ BeansJam2/Assets/Scripts/balancemovement.cs
@@ -0,0 +1,52 @@
+﻿
+using UnityEngine;
+
+public class balancemovement : MonoBehaviour {
+
+    public Rigidbody2D body;
+    public GameObject Stopper;
+   
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+
+    private void Update()
+    {
+      if (Stopper.transform.rotation.eulerAngles.z >= 177)
+        {
+            this.enabled = false;
+           
+        }
+
+        if (Stopper.transform.rotation.eulerAngles.z <= 3)
+        {
+            this.enabled = false;
+
+        }
+    }
+
+
+    void FixedUpdate() {
+
+        if (Input.GetKey("left"))
+        {
+            body.AddForce(new Vector2(-1, 1/20), ForceMode2D.Impulse);
+
+        }
+
+        if (Input.GetKey("right"))
+        {
+            body.AddForce(new Vector2(1, 1), ForceMode2D.Impulse);
+
+        }
+
+       
+
+    }
+
+   
+}
+
