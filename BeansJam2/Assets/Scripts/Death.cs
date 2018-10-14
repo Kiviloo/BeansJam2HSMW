4df58da5bfb2a6750630c080fd40ba39c5f64028
Vestalia <Vestalia95>
2018-10-14 14:18:17 +0200

    menus will be implementet

---
+++ BeansJam2/Assets/Scripts/Death.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour {
+
+	
+	void Start () {
+
+        StartCoroutine(DeathWaiter());
+    }
+
+    IEnumerator DeathWaiter()
+    {
+        yield return new WaitForSeconds(6);
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ BeansJam2/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour {
+
+	public void OnStart()
+    {
+        SceneManager.LoadScene("Ingame"); // SceneManager.LoadScene("Game");
+    }
+
+    public void OnCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
+}
